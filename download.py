a7d30a099f3061d4da24a4410ef768a4466d958e
Yadheedhya.i <79125868+Yadheedhya06@users.noreply.github.com>
2023-03-04 18:34:45 +0530

    Update download.py

--- download.py
+++ download.py
@@ -7,7 +7,7 @@ from transformers import pipeline
 
 def download_model():
     # do a dry run of loading the huggingface model, which will download weights
-    pipeline('summarization', model="braindao/Flan-T5", framework='pt',use_auth_token='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')
+    pipeline(model="braindao/Flan-T5", use_auth_token='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')
 
 if __name__ == "__main__":
     download_model()
